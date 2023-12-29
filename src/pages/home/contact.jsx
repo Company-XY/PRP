dbd73a2f6b3a2a7c19e6eaf5162414aaeb15abf5
Tokyo <119660460+Tokyo254@users.noreply.github.com>
2023-12-29 21:24:27 +0300

    review

--- src/pages/home/contact.jsx
+++ src/pages/home/contact.jsx
@@ -1,11 +1,21 @@
 
-
 const Contact = () => {
   return (
     <div className="bg-white h-screen">
-      
+           <div className=" text-black text-center">
+           <span className="text-4xl font-bold">Contact Us</span>
+           <p className="text-sm mt-3">
+            Sustainable Growth: Achieve steady and <br /> sustainable growth with our innovative solutions.
+          </p>
+          </div>
+         
+
     </div>
   );
 };
 
 export default Contact;
+
+
+
+
--- src/pages/home/footer.jsx
+++ src/pages/home/footer.jsx
@@ -6,35 +6,30 @@ const Footer = () => {
   return (
     <div className='bg-gradient-to-l from-grey to-black'>
       <div className="flex justify-center space-x-4 p-4">
-        {/* Facebook Icon */}
         <div className="text-center flex flex-col items-center">
           <div className="rounded-full bg-black p-4">
             <FaFacebook size={16} color="white" />
           </div>
         </div>
 
-        {/* Twitter Icon */}
         <div className="text-center flex flex-col items-center">
           <div className="rounded-full bg-black p-4">
             <FaTwitter size={16} color="white"  />
           </div>
         </div>
 
-        {/* Pinterest Icon */}
         <div className="text-center flex flex-col items-center">
           <div className="rounded-full bg-black p-4">
             <FaPinterest size={16} color="white"  />
           </div>
         </div>
 
-        {/* LinkedIn Icon */}
         <div className="text-center flex flex-col items-center">
           <div className="rounded-full bg-black p-4">
             <FaLinkedin size={16} color="white"  />
           </div>
         </div>
 
-        {/* Google Icon */}
         <div className="text-center flex flex-col items-center">
           <div className="rounded-full bg-black p-4">
             <FaGoogle size={16} color="white" />
