b03609a6143a7af0108e4484876cc098790aab47
Olosko23 <oloogeorge633@gmail.com>
2024-01-25 09:48:09 +0300

    'files'

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,16 @@
-import Routers from "./pages/routes/Routers";
+import Routers from "./routes/Routers";
 import { BrowserRouter } from "react-router-dom";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
 
 function App() {
   return (
     <BrowserRouter>
+      <Header />
       <Routers />
+      <Footer />
     </BrowserRouter>
   );
-} 
+}
 
 export default App;
-
--- src/routes/Routers.jsx
+++ src/routes/Routers.jsx
@@ -1,22 +1,18 @@
 import { Routes, Route } from "react-router-dom";
-import Home from "../Home";
-import Header from "../../components/Header";
-import Footer from "../../components/Footer";
-import Dashboard from "../Dashboard/mainDashboard";
-import Login from "../Auth/Login";
-import Signup from "../Auth/Signup";
+import Home from "../pages/Home";
+import Dashboard from "../pages/Dashboard/mainDashboard";
+import Login from "../pages/Auth/Login";
+import Signup from "../pages/Auth/Signup";
 
 function Routers() {
   return (
     <>
-      <Header />
       <Routes>
         <Route index element={<Home />} />
         <Route path="/dashboard" element={<Dashboard />} />
         <Route path="/login" element={<Login />} />
         <Route path="/signup" element={<Signup />} />
       </Routes>
-      <Footer />
     </>
   );
 }
